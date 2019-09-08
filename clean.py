5febf50fc8d2e480f0140fbe2a457f72cdd8bd57
blaisewang <kaito.kaito.k@gmail.com>
2019-09-08 03:14:38 +0100

    Update constant naming

--- clean.py
+++ clean.py
@@ -8,7 +8,7 @@ import csv
 import os
 import re
 
-threshold = 1.5
+THRESHOLD = 1.5
 
 DUMMY_LINE = "228.0	#	-1.0"
 
@@ -35,9 +35,9 @@ def clean_word(words):
 
 create_directory("./input")
 
-for threshold in [0, 0.5, 1.0, 1.5, 2.0]:
+for THRESHOLD in [0, 0.5, 1.0, 1.5, 2.0]:
 
-    output_path = "./input/data_%s.csv" % str(threshold).replace(".", "")
+    output_path = "./input/data_%s.csv" % str(THRESHOLD).replace(".", "")
 
     # topics processing
     with open("./data/topics.csv", "r") as topics_in, open("./data/dataset_text.txt", "r") as labels_in, open(
@@ -76,5 +76,5 @@ for threshold in [0, 0.5, 1.0, 1.5, 2.0]:
             # score parsing
             score = float(line[2].strip("\n"))
             # keep relevant labels only
-            if score >= threshold:
+            if score >= THRESHOLD:
                 writer.writerow([topics[index], clean_word(line[1].split())])
